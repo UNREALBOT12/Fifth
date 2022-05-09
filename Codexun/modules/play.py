4318a01438fab2275335e82d17d3077c481d8345
AAYUU <91039807+UNREALBOT12@users.noreply.github.com>
2022-05-09 13:05:17 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -7,7 +7,7 @@ import psutil
 import subprocess
 import requests
 import aiohttp
-import git+https://github.com/UNREALBOT12/yt-dll@master
+import https://github.com/UNREALBOT12/yt-dll@master
 import aiohttp
 import random
 
